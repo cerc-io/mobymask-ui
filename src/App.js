d8efc5212f3ec9b4382835b2db27aae79f2eb495
nabarun <nabarun@deepstacksoft.com>
2023-04-18 20:36:02 +0530

    Log decoded transaction data from invocation messages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React, { useCallback, useContext, useEffect } from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import { ethers } from "ethers";
 import { PeerContext } from "@cerc-io/react-peer";
+import { getPseudonymForPeerId } from "@cerc-io/peer";
 import "./utils/installBuffer";
 import QueryParamsRoute from "./views/RoutableArea";
 import { HashRouter } from "react-router-dom";
@@ -9,16 +11,59 @@ import CheckPhisherStatus from "./views/CheckPhisherStatus";
 import HeaderBox from "./views/HeaderBox";
 import InstallExtension from "./views/InstallExtension";
 import FooterBox from "./views/FooterBox";
-import { MOBYMASK_TOPIC } from "./utils/constants";
+import { MESSAGE_KINDS, MOBYMASK_TOPIC } from "./utils/constants";
+import { getCurrentTime } from "./utils/getCurrentTime";
+import artifacts from "./utils/artifacts.json";
+
+const contractInterface = new ethers.utils.Interface(artifacts.abi);
 
 function App() {
   const peer = useContext(PeerContext);
+  const [messages, setMessages] = useState([]);
 
   const handleTopicMessage = useCallback((peerId, data) => {
-    console.log("Message from peer:", peerId.toString())
-    console.log("Signed invocations")
-    console.log(data)
-  })
+    const { kind, message } = data;
+    const messageLogs = [`[${getCurrentTime()}] Received a message on mobymask P2P network from peer: ${peerId.toString()} (${getPseudonymForPeerId(peerId.toString())})`];
+
+    switch (kind) {
+      case MESSAGE_KINDS.INVOKE: {
+        messageLogs.push(
+          "Signed invocations:",
+          JSON.stringify(message, null, 2)
+        );
+
+        const [{ invocations: { batch: invocationsList } }] = message;
+        Array.from(invocationsList).forEach(invocation => {
+          const txData = invocation.transaction.data;
+          const decoded = contractInterface.parseTransaction({ data: txData });
+
+          messageLogs.push(`method: ${decoded.name}, value: ${decoded.args[0]}`);
+        });
+
+        break;
+      }
+
+      case MESSAGE_KINDS.REVOKE: {
+        const { signedDelegation, signedIntendedRevocation } = message;
+
+        messageLogs.push(
+          "Signed delegation:",
+          JSON.stringify(signedDelegation, null, 2),
+          "Signed intention to revoke:",
+          JSON.stringify(signedIntendedRevocation, null, 2),
+        );
+
+        break;
+      }
+
+      default:
+        break;
+    }
+
+    messageLogs.forEach(messageLog => console.log(messageLog))
+    setMessages(prevMessages => ([...prevMessages, messageLogs]))
+    console.log('------------------------------------------')
+  }, [])
 
   useEffect(() => {
     if (peer) {
