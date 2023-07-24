145b9a43911fd4824dae83b76acc58f0945bea7d
Nabarun <nabarun@deepstacksoft.com>
2023-07-24 19:11:06 +0530

    Save nitro key to localStorage at app start

--- src/atoms/nitroKeyAtom.js
+++ src/atoms/nitroKeyAtom.js
@@ -1,9 +1,6 @@
-import { ethers } from "ethers";
 import { atomWithStorage } from "jotai/utils";
-
-const wallet = ethers.Wallet.createRandom()
 
 export const nitroKeyAtom = atomWithStorage(
   "nitroKey",
-  localStorage.getItem("nitroKey") || wallet.privateKey
+  localStorage.getItem("nitroKey") || ""
 );
